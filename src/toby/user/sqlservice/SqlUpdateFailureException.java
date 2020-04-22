708c1c7aff6febab65794fbe3ce21ae52c93a9a9
Wondy Kim <wondy96@gmail.com>
2020-04-23 01:59:45 +0900

    Chapter 7
      - 7.4 인터페이스 상속을 통한 안전한 기능확

---
+++ src/toby/user/sqlservice/SqlUpdateFailureException.java
@@ -0,0 +1,20 @@
+package toby.user.sqlservice;
+
+public class SqlUpdateFailureException extends Exception {
+
+	public SqlUpdateFailureException() {
+		super();
+	}
+
+	public SqlUpdateFailureException(String message) {
+		super(message);
+	}
+
+	public SqlUpdateFailureException(Throwable cause) {
+		super(cause);
+	}
+
+	public SqlUpdateFailureException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
